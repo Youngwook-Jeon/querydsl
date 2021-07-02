953334498a0a908574bbdb9a26b655f76aadfef3
Young wook Jeon <ela87@naver.com>
2021-07-02 14:24:20 +0900

    Add Member/Team entities

--- src/test/java/com/young/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/young/querydsl/QuerydslApplicationTests.java
@@ -7,6 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+//@Commit 사용 시 롤백이 되지 않도록 함
 class QuerydslApplicationTests {
 
     @Autowired
